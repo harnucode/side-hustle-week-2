40a1dd8d28bcd9ef2a476c135952c38f19cf9846
harnucode <oyedeleanu@gmail.com>
2023-03-18 13:56:54 +0100

    this is the a code that validate voters information for elligibility

---
+++ sidehustleweek2.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $age = 18;
+    $havepvc = True;
+    $ward = 020;
+
+    if($age >= 18 and $havepvc == True and $ward = 020){
+        echo 'Voters is elligible to vote ';
+    }
+    else{
+        echo "Voters is not elligible to vote";
+
+    }
+
+
+
+?>
